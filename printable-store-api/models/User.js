03be2934cfd212e0b3e59afa0a1b7b431abad6be
desivar <jillian_desire@yahoo.es>
2025-04-10 12:18:14 -0600

    add model/User.js

---
+++ printable-store-api/models/User.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, // We'll hash this later
+  address: { type: String },
+  phone: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
